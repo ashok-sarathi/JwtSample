7e0e9334225dbac020abbed37f0d6d136ca3c4c4
ashok-sarathi <ashoksarathi004@gmail.com>
2025-01-24 15:03:06 +0530

    Fixed

---
+++ JwtSample/Data/JwtContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using JwtSample.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtSample.Data
+{
+    public class JwtContext(DbContextOptions<JwtContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    public static class ContextRegistory
+    {
+        public static IServiceCollection AddDbContextSettings(this IServiceCollection services)
+        {
+            services.AddDbContext<JwtContext>((o) =>
+            {
+                o.UseInMemoryDatabase("JwtDb");
+            });
+            return services;
+        }
+    }
+}
--- JwtSample/Program.cs
+++ JwtSample/Program.cs
@@ -1,3 +1,8 @@
+using JwtSample.Data;
+using JwtSample.Entity;
+using JwtSample.Helper;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +10,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+    });
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+
+                    }
+                });
+});
+builder.Services.AddDbContextSettings();
 
 var app = builder.Build();
 
@@ -19,7 +51,43 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthMiddleware();
 
 app.MapControllers();
+
+using var serviceScope = app.Services.CreateScope();
+var dbContext = serviceScope.ServiceProvider.GetRequiredService<JwtContext>();
+dbContext
+    .Set<User>()
+    .AddRange(
+        new List<User>()
+        {
+            new User()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Admin",
+                Email = "admin@demo.com",
+                Password = "admin@demo.com",
+                UserType = UserType.Admin
+            },
+            new User()
+            {
+                Id = Guid.NewGuid(),
+                Name = "User",
+                Email = "user@demo.com",
+                Password = "user@demo.com",
+                UserType = UserType.User
+            },
+            new User()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Auditor",
+                Email = "auditor@demo.com",
+                Password = "auditor@demo.com",
+                UserType = UserType.Auditor
+            }
+        });
+
+dbContext.SaveChanges();
 
 app.Run();
